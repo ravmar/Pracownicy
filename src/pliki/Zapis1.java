25505cb25cd1f8bc3c322e7f31da25c4c5d760f0
kurs <kurs@kurs-Komputer>
2018-04-12 15:31:01 +0200

    wersja z godzina i data

--- src/pliki/Zapis1.java
+++ src/pliki/Zapis1.java
@@ -2,6 +2,9 @@ package pliki;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
 
 public class Zapis1 {
 
@@ -10,6 +13,10 @@ public class Zapis1 {
 		try {
 			PrintWriter out = new PrintWriter("C:/Users/kurs/Desktop/plik1.txt");
 			out.println("Zapis do pliku");
+			out.println("Data: " + LocalDate.now());
+			out.println("Godzina: " + LocalTime.now());
+			out.println("Pełna data: "+ZonedDateTime.now());
+			out.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

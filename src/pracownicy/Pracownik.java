7148a58542d2f006a1262a054753a307756aee7c
kurs <kurs@kurs-Komputer>
2018-04-12 16:42:42 +0200

    wersja 3 - hashCode i equals

--- src/pracownicy/Pracownik.java
+++ src/pracownicy/Pracownik.java
@@ -162,6 +162,79 @@ public class Pracownik {
 				+ ", data=" + data + ", pensja=" + pensja + ", telefon=" + telefon + ", departament=" + departament
 				+ ", adres=" + adres + ", miasto=" + miasto + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adres == null) ? 0 : adres.hashCode());
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((departament == null) ? 0 : departament.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((imie == null) ? 0 : imie.hashCode());
+		result = prime * result + ((miasto == null) ? 0 : miasto.hashCode());
+		result = prime * result + ((nazwisko == null) ? 0 : nazwisko.hashCode());
+		result = prime * result + pensja;
+		result = prime * result + ((stanowisko == null) ? 0 : stanowisko.hashCode());
+		result = prime * result + ((telefon == null) ? 0 : telefon.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pracownik other = (Pracownik) obj;
+		if (adres == null) {
+			if (other.adres != null)
+				return false;
+		} else if (!adres.equals(other.adres))
+			return false;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (departament == null) {
+			if (other.departament != null)
+				return false;
+		} else if (!departament.equals(other.departament))
+			return false;
+		if (id != other.id)
+			return false;
+		if (imie == null) {
+			if (other.imie != null)
+				return false;
+		} else if (!imie.equals(other.imie))
+			return false;
+		if (miasto == null) {
+			if (other.miasto != null)
+				return false;
+		} else if (!miasto.equals(other.miasto))
+			return false;
+		if (nazwisko == null) {
+			if (other.nazwisko != null)
+				return false;
+		} else if (!nazwisko.equals(other.nazwisko))
+			return false;
+		if (pensja != other.pensja)
+			return false;
+		if (stanowisko == null) {
+			if (other.stanowisko != null)
+				return false;
+		} else if (!stanowisko.equals(other.stanowisko))
+			return false;
+		if (telefon == null) {
+			if (other.telefon != null)
+				return false;
+		} else if (!telefon.equals(other.telefon))
+			return false;
+		return true;
+	}
 	
 	
 	

4d22e5d33ad58dd33965c37b5034329abaf7530c
kurs <kurs@kurs-Komputer>
2018-04-13 10:06:55 +0200

    wczytywanie pracownikow

--- src/pracownicy/ObslugaCSV.java
+++ src/pracownicy/ObslugaCSV.java
@@ -2,6 +2,8 @@ package pracownicy;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -15,7 +17,14 @@ public class ObslugaCSV {
 		try(Scanner sc = new Scanner(plik)) {
 			while(sc.hasNextLine()) {
 				String linia = sc.nextLine();
-				System.out.println(linia);
+				String[] pola = linia.split(";");
+				
+				int id = Integer.parseInt(pola[0]);
+				LocalDate data = LocalDate.parse(pola[4]);
+				BigDecimal pensja = new BigDecimal(pola[5]);
+				
+				Pracownik pracownik = new Pracownik(id, pola[1], pola[2], pola[3], data, pensja, pola[6], pola[7], pola[8], pola[9]);
+				lista.add(pracownik);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();

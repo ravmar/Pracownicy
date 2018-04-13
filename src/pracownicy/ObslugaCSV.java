de77a61af22c360b5edd8dbc242f384bf1290c21
kurs <kurs@kurs-Komputer>
2018-04-13 09:49:54 +0200

    otwarcie pliku i wczytywanie wierszy

--- src/pracownicy/ObslugaCSV.java
+++ src/pracownicy/ObslugaCSV.java
@@ -1,13 +1,25 @@
 package pracownicy;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ObslugaCSV {
 	
 	public static List<Pracownik> odczytaj(String sciezka){
 		List<Pracownik> lista = new ArrayList<>(); 
 		
+		File plik = new File(sciezka);
+		try(Scanner sc = new Scanner(plik)) {
+			while(sc.hasNextLine()) {
+				String linia = sc.nextLine();
+				System.out.println(linia);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		
 		return lista;		
 	}

ea8200cc72a7f20ceb2291a9722e118b3db16dac
kurs <kurs@kurs-Komputer>
2018-04-13 09:33:13 +0200

    poczatek

---
+++ src/pracownicy/ObslugaCSV.java
@@ -0,0 +1,15 @@
+package pracownicy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObslugaCSV {
+	
+	public static List<Pracownik> odczytaj(String sciezka){
+		List<Pracownik> lista = new ArrayList<>(); 
+		
+		
+		return lista;		
+	}
+
+}
---
+++ src/pracownicy/TestOdczytu.java
@@ -0,0 +1,22 @@
+package pracownicy;
+
+import java.util.List;
+
+public class TestOdczytu {
+
+	public static void main(String[] args) {
+		
+		System.out.println("poczatek czytania");
+		List<Pracownik> lista = ObslugaCSV.odczytaj("pracownicy.csv");
+		
+		System.out.println("wczytano " + lista.size() + " pracowników");
+		for (Pracownik pracownik : lista) {
+			System.out.println(pracownik);
+		}
+		System.out.println("koniec");
+		
+		
+
+	}
+
+}
